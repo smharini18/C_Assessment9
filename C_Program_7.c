d7329b21b947e5da18df4a6ab60c06c5e84f33f6
smharini18 <smharini18@gmail.com>
2026-07-08 23:42:42 +0530

    Create C_Program_7.c
    This program takes five integers as input, displays them, and sorts them in descending order.

---
+++ C_Program_7.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+int main()
+{
+    int num[5];
+    int x,y,temp;
+    printf("Enter the Number : ");
+    for(x=0;x<5;x++)
+    {
+        scanf("%d",&num[x]);
+    }
+    printf("\nThe Same Number : ");
+    for(x=0;x<5;x++)
+    {
+        printf("%d",num[x]);
+    }
+    for(x=0;x<5;x++)
+    {
+        for(y=x+1;y<5;y++)
+        {
+            if(num[x]>num[y])
+            {
+                temp=num[x];
+                num[x]=num[y];
+                num[y]=temp;
+            }
+        }
+    }
+    printf("\nDescending order : ");
+    for(x=4;x>=0;x--)
+    {
+        printf("%d",num[x]);
+    }
+}
