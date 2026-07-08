98d44a4425b28a40c828ec86fce8ea92dda5674c
smharini18 <smharini18@gmail.com>
2026-07-08 23:45:01 +0530

    Implement program to input and filter odd numbers

---
+++ C_Program_9.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    int num[5],j;
+    printf("Enter the Number : ");
+    for(j=0;j<5;j++)
+    {
+        scanf("%d",&num[j]);
+    }
+    printf("\nThe Same Number : ");
+    for(j=0;j<5;j++)
+    {
+        printf("%d",num[j]);
+    }
+    printf("\nAfter Removing Even Number : ");
+    for(j=0;j<5;j++)
+    {
+        if(num[j]%2==1)
+        {
+            printf("%d",num[j]);
+        }
+    }
+}
